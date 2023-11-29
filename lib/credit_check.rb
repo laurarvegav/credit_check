c6812d2d1a912e9cd5e63fd73d1b3dd140e580ec
Laura Vega Vargas <ing.laurarvega@gmail.com>
2023-11-29 11:48:55 -0700

    Add a short version for the method

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,64 +1,105 @@
 # I need to create a method where the card number is converted to an array
-# and multiply by 2 the elements stored in the uneven indexes of that
-# array. Then evaluate the elements that are >=10, add both digits and
-# store that result in the same index. Then sum all the elements of
-# the last array and find if that sum result is divisible by 10.
+# that stores integers, then multiply by 2 the elements stored in the uneven 
+# indexes of that array. Then evaluate the elements that are >=10, add both 
+# digits and store that result in the same index. Then sum all the elements
+# of the last array and find if that sum result is divisible by 10.
 
-# # Your Luhn Algorithm Here
-def Luhn_Algorithm(card_number)
-    #Convert that string to an array, making each number an index
-    card_numbers = card_number.split("")
-    #Convert each element on the array to an integer
-    card_numbers = card_numbers.map{|x| x.to_i}
+# LONG Luhn Algorithm 
+# def Luhn_Algorithm(card_number) 
+#     #Convert that string to an array, making each number an index
+#     card_numbers = card_number.split("")
+#     #Convert each element on the array to an integer
+       
+#     card_numbs = card_numbers.map{|x| x.to_i} 
+#     p card_numbs
+
+#     n = card_number.length
+#     n.times { |x|
+#         card_numbers[x] = (card_numbers[x].to_i)
+#     }
     
-    # Multiply every other element by 2, starting by the first, (2* uneven element)
-    n = card_number.length
-    card_multiplication = Array.new
-    n.times {|x| 
-        if x.even?
-            card_multiplication[x] = card_numbers[x] * 2
-        else
-            card_multiplication[x] = card_numbers[x]
-        end
-        x += 1
-    }
+#     # Multiply every other element by 2, starting by the first, (2* uneven element)
+#     card_multiplication = Array.new
+#     n.times {|x| 
+#         if x.even?
+#             card_multiplication[x] = card_numbers[x] * 2
+#         else
+#             card_multiplication[x] = card_numbers[x]
+#         end
+#         x += 1
+#     }
 
-    # Evaluate the elements that are >=10, add both digits and store that result 
-    # in the same index.
-    card_additions = Array.new
-    n.times {|x|
-        if card_multiplication[x] >= 10
-            card_additions[x] = ((card_multiplication[x]/10) + (card_multiplication[x]%10))
-            x += 1
-        else 
-            card_additions[x] = card_multiplication[x]
-        end
-    }
-    # sum all the elements of the last array 
-    card_addition = card_additions.sum
+#     # Evaluate the elements that are >=10, add both digits and store that result 
+#     # in the same index.
+#     card_additions = Array.new
+#     n.times {|x|
+#         if card_multiplication[x] >= 10
+#             card_additions[x] = ((card_multiplication[x]/10) + (card_multiplication[x]%10))
+#             x += 1
+#         else 
+#             card_additions[x] = card_multiplication[x]
+#         end
+#     }
+#     # sum all the elements of the last array 
+#     card_addition = card_additions.sum
     
-    #Evaluate if that sum result is divisible by 10.
-    if (card_addition%10) == 0
-        puts "The number #{card_number} is valid!"
-    else
-        puts "The number #{card_number} is invalid!"
-    end
-end
+#     #Evaluate if that sum result is divisible by 10.
+#     if (card_addition%10) == 0
+#         puts "The number #{card_number} is valid!"
+#     else
+#         puts "The number #{card_number} is invalid!"
+#     end
+# end
 
-card_number = "5541808923795240"
-Luhn_Algorithm(card_number)
 
-card_number = "4024007136512380"
-Luhn_Algorithm(card_number)
+# # SHORT Luhn Algorithm 
+# def Luhn_Algorithm(card_number) 
+#     #Convert that string to an integer
+#     card_number = card_number.to_i
+#     #Store integer in an array 
+#     card_numbers = card_number.digits.reverse
+         
+#     # Multiply every other element by 2, starting by the first, 
+#     # (2* even-index element)
+#     card_multiplication = Array.new
+#     card_numbers.each_index do |i|
+#         if i.even?
+#             card_multiplication << (card_numbers[i] *2)
+#         else
+#             card_multiplication << card_numbers[i]
+#         end
+#     end
 
-card_number = "6011797668867828"
-Luhn_Algorithm(card_number)
+#     # Evaluate the elements that are >=10, add both digits and store that result 
+#     # in the same index.
+#     card_additions = Array.new
+#     card_multiplication.each do |n|
+#         if n >= 10
+#             card_additions << n.digits.sum
+#         else
+#             card_additions << n
+#         end
+#     end
+   
+#     # sum all the elements of the last array 
+#     card_addition = card_additions.sum
+    
+#     #Evaluate if that sum result is divisible by 10.
+#     if (card_addition%10) == 0
+#         puts "The number #{card_number} is valid!"
+#     else
+#         puts "The number #{card_number} is invalid!"
+#     end
+# end
 
-card_number = "5541801923795240"
-Luhn_Algorithm(card_number)
+Luhn_Algorithm("5541808923795240")
 
-card_number = "4024007106512380"
-Luhn_Algorithm(card_number)
+Luhn_Algorithm("4024007136512380")
 
-card_number = "6011797668868728"
-Luhn_Algorithm(card_number)
+Luhn_Algorithm("6011797668867828")
+
+Luhn_Algorithm("5541801923795240")
+
+Luhn_Algorithm("4024007106512380")
+
+Luhn_Algorithm("6011797668868728")
